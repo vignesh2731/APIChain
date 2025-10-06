eb4300f493972af6c07084aa652bd5c6ca5b57c1
sreevignesh2731 <sreevignesh27@gmail.com>
2025-10-06 23:42:04 +0530

    Init

---
+++ apps/web-hook/src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import db from '@repo/db/client'
+const app = express();
+app.use(express.json());
+
+app.post("/hooks/catch/:userId/:zapId",async(req,res)=>{
+    const { userId, zapId} = req.params;
+    const body = req.body
+    await db.$transaction(async(tx)=>{
+        const response = await tx.zapRun.create({
+            data:{
+                zapId:zapId,
+                metadata:body
+            }
+        })
+        await tx.zapRunOutbox.create({
+            data:{
+            zapRunId:response.id
+            }
+        })
+    })
+    console.log("Stored inside db done");
+    res.json({
+        msg:"Query executed"
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("Listening to port 3000")
+})

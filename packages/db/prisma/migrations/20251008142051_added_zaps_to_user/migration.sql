8b3be5eda216e0ac2e549752caec5971f9c40ddb
sreevignesh2731 <sreevignesh27@gmail.com>
2025-10-08 21:43:13 +0530

    Added a primary backend for creating zaps

---
+++ packages/db/prisma/migrations/20251008142051_added_zaps_to_user/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Zap" ADD COLUMN     "userId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "Zap" ADD CONSTRAINT "Zap_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;

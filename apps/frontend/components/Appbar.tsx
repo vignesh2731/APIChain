62eb0c5df379439d35ab9d3c5f2400b4e5a75ba4
sreevignesh2731 <sreevignesh27@gmail.com>
2025-10-12 20:49:42 +0530

    Updated the nav bar

--- apps/frontend/components/Appbar.tsx
+++ apps/frontend/components/Appbar.tsx
@@ -2,9 +2,22 @@
 import { useRouter } from "next/navigation";
 import { Primary } from "./buttons/Primary";
 import { Secondary } from "./buttons/Secondary";
+import { useEffect, useState } from "react";
 
 export function Appbar(){
+    const [mount,setMount] = useState(false);
+    const [token,setToken] = useState<string|null>();
+    useEffect(()=>{
+        setMount(true);
+        setToken(localStorage.getItem("token"));
+    },[])
     const router = useRouter();
+    
+    if(!mount){
+        return <div>
+
+        </div>
+    }
     return (
         <div className="flex flex-col justify-center px-10 border-b border-slate-300 h-20">
             <div className="flex justify-between">
@@ -13,15 +26,22 @@ export function Appbar(){
                 }}>
                     Zapier 
                 </div>
-                <div className="flex gap-10">
+                {!token && <div className="flex gap-10">
                     <Secondary label="Contact Sales" onClickAction={()=>{}}className = "text-sm"/>
-                    <Secondary label="Login" onClickAction={()=>{
+                    {<Secondary label="Login" onClickAction={()=>{
                         router.push("/login")
-                    }} className="text-sm" />
+                    }} className="text-sm" />}
                     <Primary label="Signup" onClickAction={()=>{
                         router.push("/signup")
                     }} className="w-24 h-8 font-semibold text-sm hover:shadow-md"/>                                          
-                </div>
+                </div>}
+                {token && <div>
+                    <Secondary label="Logout" className="border border-slate-200 p-2 rounded-2xl" onClickAction={()=>{
+                        localStorage.removeItem("token");
+                        setToken(null);
+                        router.push("/");
+                    }}/>
+                    </div>}
             </div>
         </div>
     )
